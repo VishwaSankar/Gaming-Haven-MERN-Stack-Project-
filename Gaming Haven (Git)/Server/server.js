c984bce92d37047c80775209e98330d88a839587
VishwaSankar <136370992+VishwaSankar@users.noreply.github.com>
2024-02-22 14:55:46 +0530

    Update server.js

--- Gaming Haven (Git)/Server/server.js
+++ Gaming Haven (Git)/Server/server.js
@@ -25,7 +25,7 @@ const connect= async ()=> {try{
 app.use(sessionMiddleware);
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:3000",credentials:true}))
+app.use(cors({origin:"https://gaming-haven-frontend.vercel.app",credentials:true,methods:["POST","GET"]}))
 app.use("/auth",authRoute)
 app.use("/users",userRoute)
 // app.use("/games",gamesRoute)
